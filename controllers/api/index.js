a9fa9d9f5690c810d0e505e2c792cf4087441559
Rashida Kapadia <rashidamk21@gmail.com>
2022-12-21 13:14:55 -0600

    Removed updateRoutes as not being used

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogpostRoutes = require('./blogpostRoutes');
 const commentRoutes = require('./commentRoutes');
-// const updateRoutes = require('./updateRoutes')
 
 router.use('/users', userRoutes);
 router.use('/blogposts', blogpostRoutes);
 router.use('/comments', commentRoutes);
-// router.use('/updates', updateRoutes);
 
 module.exports = router;
